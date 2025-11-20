57bbfc7e018c127850f6754e1be3dfa2cf03df50
kareem-dev-del <krym33632@gmail.com>
2025-11-20 22:36:44 +0200

    Update

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product 
 from .models  import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import messages 
 
 # هذه الدالة مسؤولة عن إرجاع session id الخاص بالمستخدم
 def _cart_id(request):
@@ -52,6 +53,32 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
+def remove_cart(request, product_id):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        product = Product.objects.get(id=product_id)
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+
+    except (Cart.DoesNotExist, CartItem.DoesNotExist, Product.DoesNotExist):
+        pass  # لو الحاجة مش موجودة، ما يعملش حاجة
+
+    return redirect('cart')
+   
+def remove_cart_item(request, product_id):
+    cart = Cart.objects.get(cart_id=_cart_id(request))
+    product = get_object_or_404(Product,id=product_id)
+    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item.delete()
+    return redirect('cart')
+
+
+
 
 #3# دالة عرض السلة
 def cart(request, total=0,quantity=0,cart_item=None):
@@ -61,6 +88,8 @@ def cart(request, total=0,quantity=0,cart_item=None):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total + tax    
     except ObjectDoesNotExist:
       pass
 
@@ -70,5 +99,7 @@ def cart(request, total=0,quantity=0,cart_item=None):
         'total': total,
         'quantity': quantity,
        'cart_items' : cart_items,
+       'tax' : tax,
+       'grand_total' : grand_total,
     }
     return render(request, 'store/cart.html',context)
