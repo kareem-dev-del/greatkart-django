c1c3adb0dc40e1312f08f510b1a187588bda80a9
kareem-dev-del <krym33632@gmail.com>
2025-11-23 00:11:48 +0200

    pdate

--- carts/views.py
+++ carts/views.py
@@ -41,7 +41,7 @@ def add_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
-        ### لو المنتج لسه أول مرة يدخل السلة → ننشئ عنصر جديد
+       # ### لو المنتج لسه أول مرة يدخل السلة → ننشئ عنصر جديد
         cart_item = CartItem.objects.create(
             product=product,
             quantity=1,
