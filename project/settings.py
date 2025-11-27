d7118ff5d8bef195a3ca8ee199408ed8d6e8bce1
kareem-dev-del <krym33632@gmail.com>
2025-11-27 20:35:30 +0200

    Update

--- project/settings.py
+++ project/settings.py
@@ -66,7 +66,8 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.contex_processors.menu_links',
-                'carts.coutext_processoer.counter',
+               'carts.context_processors.counter',
+
             ],
         },
     },
