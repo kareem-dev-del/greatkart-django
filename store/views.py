ad8c75b040069f45da57713dac4c3306e9b928d4
kareem-dev-del <krym33632@gmail.com>
2025-11-28 21:17:23 +0200

    Update

--- store/views.py
+++ store/views.py
@@ -19,7 +19,7 @@ def store(request,category_slug=None):
         products = Product.objects.filter(category=categories, is_available=True)
     else:
         products = Product.objects.all().filter(is_available=True)
-    paginator = Paginator(products, 6)
+    paginator = Paginator(products, 20)
     page = request.GET.get('page')
     paged_product = paginator.get_page(page)
    
