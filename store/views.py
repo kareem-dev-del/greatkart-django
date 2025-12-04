81898c0fe2fbb272fe9466ec1b2ad4f64d8694d2
kareem-dev-del <krym33632@gmail.com>
2025-12-04 16:24:22 +0200

    Update

--- store/views.py
+++ store/views.py
@@ -6,6 +6,8 @@ from carts.views import _cart_id
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 
 
 
@@ -53,10 +55,19 @@ def product_detail(request, category_slug, product_slug):
         'in_cart': in_cart
 
     }
-    
-
-
-
-
-
     return render(request, 'store/product_detail.html', context )
+
+def search(request):
+    products = [] 
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword'].strip()
+        if keyword:
+          products = Product.objects.order_by('-created_date').filter( Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+          product_count = products.count()
+
+            
+    context ={
+        'products':products,
+        'product_count':product_count,
+    }          
+    return render(request, 'store/store.html',context)
